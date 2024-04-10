488b9951c998bba054d882ee880d89dcf65e7b54
Aiman-48 <160266058+Aiman-48@users.noreply.github.com>
2024-04-10 16:00:01 -0700

    new code

--- index.ts
+++ index.ts
@@ -36,8 +36,8 @@ let user_answer = await inquirer.prompt(
 ]
 )
 
-let fromAmount = currency[user_answer.from]
-let toAmount = currency[user_answer.to]
+let fromAmount = currency[user_answer.from] // exchange rate
+let toAmount = currency[user_answer.to]// exchange rate
 let amount = user_answer.amount
 let baseAmount = amount / fromAmount // USD base  currency
 let convertedAmount = baseAmount * toAmount
